3d8b9e7301095c2dfa90a3bbe8536019c3e14d6e
Paula Castillejo Bravo <Paula.Castillejo1@alu.uclm.es>
2024-07-12 22:18:43 +0200

    canciones viaje 1

--- src/data.js
+++ src/data.js
@@ -29,6 +29,57 @@ import LoveAgainImagen from './img/LoveAgain.jpg'
 import EyesDontLieAudio from './music/EyesDontLie.mp3'
 import EyesDontLieImagen from './img/EyesDontLie.jpg'
 
+import AQuienLeImportaAudio from './music/AQuienLeImporta.mp3'
+import AQuienLeImportaImagen from './img/AQuienLeImporta.jpg'
+
+import SoyRebeldeAudio from './music/SoyRebelde.mp3'
+import SoyRebeldeImagen from './img/SoyRebelde.jpeg'
+
+import TheFinalCountdownAudio from './music/TheFinalCountdown.mp3'
+import TheFinalCountdownImagen from './img/TheFinalCountdown.jpg'
+
+import LibertadAudio from './music/Libertad.mp3'
+import LibertadImagen from './img/Libertad.jpg'
+
+import YoContigoTuConmigoAudio from './music/YoContigoTuConmigo.mp3'
+import YoContigoTuConmigoImagen from './img/YoContigoTuConmigo.jpg'
+
+import VentePaAcaAudio from './music/VentePaCa.mp3'
+import VentePaAcaImagen from './img/VentePaAca.jpg'
+
+import UptownFunkAudio from './music/UptownFunk.mp3'
+import UptownFunkImagen from './img/UptownFunk.jpeg'
+
+import StarsInTheSkyAudio from './music/StarsInTheSky.mp3'
+import StarsInTheSkyImagen from './img/StarsInTheSky.jpeg'
+
+import HumanAudio from './music/Human.mp3'
+import HumanImagen from './img/Human.jpeg'
+
+import MundoDeCarameloAudio from './music/MundoDeCaramelo.mp3'
+import MundoDeCarameloImagen from './img/MundoDeCaramelo.jpg'
+
+import HoldingOutForAHeroAudio from './music/HoldingOutForAHero.mp3'
+import HoldingOutForAHeroImagen from './img/HoldingOutForAHero.jpeg'
+
+import YoruNiKakeruAudio from './music/YoruNiKakeru.mp3'
+import YoruNiKakeruImagen from './img/YoruNiKakeru.png'
+
+import TryEverythingAudio from './music/TryEverything.mp3'
+import TryEverythingImagen from './img/TryEverything.jpeg'
+
+import SmellsLikeTeenSpiritAudio from './music/SmellsLikeTeenSpirit.mp3'
+import SmellsLikeTeenSpiritImagen from './img/SmellsLikeTeenSpirit.jpg'
+
+import GoodTonightAudio from './music/GoodTonight.mp3'
+import GoodTonightImagen from './img/GoodTonight.jpg'
+
+import MagicAudio from './music/Magic.mp3'
+import MagicImagen from './img/Magic.webp'
+
+import MrBlueSkyAudio from './music/MrBlueSky.mp3'
+import MrBlueSkyImagen from './img/MrBlueSky.jpeg'
+
 import { v4 as uuidv4 } from "uuid"; 
 function chillHop() { 
 return [ 
@@ -42,6 +93,24 @@ return [
 	active: true, 
 	}, 
 	{ 
+	name: "I kissed a girl ", 
+	cover:  KissedAGirlImagen, 
+	artist: "Katy Perry", 
+	audio: KissedAGirlAudio, 
+	color: ["#205950", "#2ab3bf"], 
+	id: uuidv4(), 
+	active: false, 
+	}, 
+	{ 
+	name: "The final countdown", 
+	cover: TheFinalCountdownImagen, 
+	artist: "Europa", 
+	audio: TheFinalCountdownAudio, 
+	color: ["#205950", "#2ab3bf"], 
+	id: uuidv4(), 
+	active: false, 
+	},
+	{ 
 	name: "La Dama del Mar", 
 	cover: LaDamaDelMarImagen, 
 	artist: "Mago de Oz", 
@@ -50,6 +119,24 @@ return [
 	id: uuidv4(), 
 	active: false, 
 	}, 
+	{ 
+	name: "Older", 
+	cover: OlderImagen, 
+	artist: "Isabel LaRosa", 
+	audio: OlderAudio, 
+	color: ["#EF8EA9", "#ab417f"], 
+	id: uuidv4(), 
+	active: false, 
+	}, 
+	{ 
+	name: "SoyRebelde", 
+	cover: SoyRebeldeImagen, 
+	artist: "Jeanette", 
+	audio: SoyRebeldeAudio, 
+	color: ["#205950", "#2ab3bf"], 
+	id: uuidv4(), 
+	active: false, 
+	},
 	{ 
 	name: "Stay", 
 	cover: StayImagen, 
@@ -65,6 +152,15 @@ return [
 	artist: "Carlos Baute", 
 	audio: ColgandoEnTusManosAudio, 
 	color: ["#EF8EA9", "#ab417f"], 
+	id: uuidv4(), 
+	active: false, 
+	}, 
+	{ 
+	name: "Love Again", 
+	cover: LoveAgainImagen, 
+	artist: "The Kid LAROI", 
+	audio: LoveAgainAudio, 
+	color: ["#CD607D", "#c94043"], 
 	id: uuidv4(), 
 	active: false, 
 	}, 
@@ -86,34 +182,6 @@ return [
 	id: uuidv4(), 
 	active: false, 
 	},
-
-	{ 
-	name: "I kissed a girl ", 
-	cover:  KissedAGirlImagen, 
-	artist: "Katy Perry", 
-	audio: KissedAGirlAudio, 
-	color: ["#205950", "#2ab3bf"], 
-	id: uuidv4(), 
-	active: true, 
-	}, 
-	{ 
-	name: "Older", 
-	cover: OlderImagen, 
-	artist: "Isabel LaRosa", 
-	audio: OlderAudio, 
-	color: ["#EF8EA9", "#ab417f"], 
-	id: uuidv4(), 
-	active: false, 
-	}, 
-	{ 
-	name: "Love Again", 
-	cover: LoveAgainImagen, 
-	artist: "The Kid LAROI", 
-	audio: LoveAgainAudio, 
-	color: ["#CD607D", "#c94043"], 
-	id: uuidv4(), 
-	active: false, 
-	}, 
 	{ 
 	name: "eyes don't lie", 
 	cover: EyesDontLieImagen, 
@@ -124,23 +192,140 @@ return [
 	active: false, 
 	}, 
 	{ 
-	name: "Volaré", 
-	cover: VolareImagen, 
-	artist: "Brave", 
-	audio: VolareAudio, 
+	name: "A quien le importa", 
+	cover: AQuienLeImportaImagen, 
+	artist: "Fangoria ", 
+	audio: AQuienLeImportaAudio, 
 	color: ["#CD607D", "#c94043"], 
 	id: uuidv4(), 
 	active: false, 
 	}, 
 	{ 
-	name: "Enemy", 
-	cover: EnemyImagen, 
-	artist: "Island Rhythms", 
-	audio: EnemyAudio, 
+	name: "Libertad", 
+	cover: LibertadImagen, 
+	artist: "Nil Moliner", 
+	audio: LibertadAudio, 
 	color: ["#205950", "#2ab3bf"], 
 	id: uuidv4(), 
 	active: false, 
 	}, 
+	{ 
+	name: "Yo Contigo, Tú Conmigo", 
+	cover: YoContigoTuConmigoImagen, 
+	artist: "Morat", 
+	audio: YoContigoTuConmigoAudio, 
+	color: ["#205950", "#2ab3bf"], 
+	id: uuidv4(), 
+	active: false, 
+	}, 
+	{ 
+	name: "Vente Pa Ca", 
+	cover: VentePaAcaImagen, 
+	artist: "Ricky Martin", 
+	audio: VentePaAcaAudio, 
+	color: ["#205950", "#2ab3bf"], 
+	id: uuidv4(), 
+	active: false, 
+	},
+	{ 
+	name: "Uptown Funk", 
+	cover: UptownFunkImagen, 
+	artist: "Bruno Mars", 
+	audio: UptownFunkAudio, 
+	color: ["#205950", "#2ab3bf"], 
+	id: uuidv4(), 
+	active: false, 
+	},
+	{ 
+	name: "Stars in the sky", 
+	cover: StarsInTheSkyImagen, 
+	artist: "Kid Cudi", 
+	audio: StarsInTheSkyAudio, 
+	color: ["#205950", "#2ab3bf"], 
+	id: uuidv4(), 
+	active: false, 
+	},
+	{ 
+	name: "Human", 
+	cover: HumanImagen, 
+	artist: "Christina Perri", 
+	audio: HumanAudio, 
+	color: ["#205950", "#2ab3bf"], 
+	id: uuidv4(), 
+	active: false, 
+	},
+	{ 
+	name: "Mundo de caramelo", 
+	cover: MundoDeCarameloImagen, 
+	artist: "Danna Paola", 
+	audio: MundoDeCarameloAudio, 
+	color: ["#205950", "#2ab3bf"], 
+	id: uuidv4(), 
+	active: false, 
+	},
+	{ 
+	name: "Holding out for a Hero", 
+	cover: HoldingOutForAHeroImagen, 
+	artist: "Bonnie Tyler", 
+	audio: HoldingOutForAHeroAudio, 
+	color: ["#205950", "#2ab3bf"], 
+	id: uuidv4(), 
+	active: false, 
+	},
+	{ 
+	name: "Yoru ni Kakeru", 
+	cover: YoruNiKakeruImagen, 
+	artist: "YOASOBI", 
+	audio: YoruNiKakeruAudio, 
+	color: ["#205950", "#2ab3bf"], 
+	id: uuidv4(), 
+	active: false, 
+	},
+	{ 
+	name: "Try Everything", 
+	cover: TryEverythingImagen, 
+	artist: "Shakira", 
+	audio: TryEverythingAudio, 
+	color: ["#205950", "#2ab3bf"], 
+	id: uuidv4(), 
+	active: false, 
+	},
+	{ 
+	name: "Smells like teen spirit", 
+	cover: SmellsLikeTeenSpiritImagen, 
+	artist: "Nirbana", 
+	audio: SmellsLikeTeenSpiritAudio, 
+	color: ["#205950", "#2ab3bf"], 
+	id: uuidv4(), 
+	active: false, 
+	},
+	{ 
+	name: "Good tonight", 
+	cover: GoodTonightImagen, 
+	artist: "Daniel Pemberton", 
+	audio: GoodTonightAudio, 
+	color: ["#205950", "#2ab3bf"], 
+	id: uuidv4(), 
+	active: false, 
+	},
+	{ 
+	name: "Magic", 
+	cover: MagicImagen, 
+	artist: "Pilot", 
+	audio: MagicAudio, 
+	color: ["#205950", "#2ab3bf"], 
+	id: uuidv4(), 
+	active: false, 
+	},
+	{ 
+	name: "Mr blue sky", 
+	cover: MrBlueSkyImagen, 
+	artist: "???", 
+	audio: MrBlueSkyAudio, 
+	color: ["#205950", "#2ab3bf"], 
+	id: uuidv4(), 
+	active: false, 
+	},
 ]; 
 } 
 
